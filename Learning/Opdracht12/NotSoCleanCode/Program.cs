6a6c86e5f922e9b2b40835007431eaa493434a61
Daan <145553717+SupahGuy@users.noreply.github.com>
2024-12-18 15:36:53 +0100

    CleanCode is clean

--- Learning/Opdracht12/NotSoCleanCode/Program.cs
+++ Learning/Opdracht12/NotSoCleanCode/Program.cs
@@ -1,265 +1,201 @@
-﻿Calculator();
-
-void Calculator()
+﻿
+public class Calculator
 {
-    while (true)
+    public static void RunCalculator()
     {
-        Console.WriteLine("Complexere Rekenmachine");
-        Console.WriteLine("Kies een operatie: +, -, *, /, % (modulo), 18+ (kassa)");
-        
-        string operation = Console.ReadLine();
-        double number1 = 0;
-        double number2 = 0;
-        
-        Console.WriteLine("Voer het eerste getal in:");
-        
-        //User input1
-        string input1 = Console.ReadLine();
-        
-        
-        if (string.IsNullOrWhiteSpace(input1))
+        while (true)
         {
-            Console.WriteLine("Ongeldige invoer, geen getal.");
-            continue;
-        }
+            Console.WriteLine("Complexere Rekenmachine");
+            Console.WriteLine("Kies een operatie: +, -, *, /, % (modulo), 18+ (kassa)");
+            string operation = Console.ReadLine();
+            
+            //inputvalidNumber1 en inputvalidNumber2 wordt validNumber1 en validNumber2 wanneer inputNumber geldig is.
+            //dit komt omdat inputvalidNumber1 en 2 een string is zodat het bepaalde functies uit kan voeren.
+            
+            double validNumber1 = 0;
+            double validNumber2 = 0;
+            
+            bool[] hasDecimalPoint = { false, false };
+            
+            Console.WriteLine("Voer het eerste getal in:");
+            
+            string inputvalidNumber1 = Console.ReadLine();
 
-        bool[] hasDecimalPoint = { false, false };
-        for (int i = 0; i < input1.Length; i++)
-        {
-            if (input1[i] == '.')
-            {
-                if (hasDecimalPoint[0])
-                {
-                    Console.WriteLine("Ongeldige invoer, geen getal.");
-                }
-                hasDecimalPoint[0] = true;
-            }
-            else if (input1[i] == '-')
-            {
-                if (i != 0)
-                {
-                    Console.WriteLine("Ongeldige invoer, geen getal.");
-                }
-            }
-            else if (!char.IsDigit(input1[i]))
-            {
-                Console.WriteLine("Ongeldige invoer, geen getal.");
-            }
-        }
-        
-        number1 = Convert.ToDouble(input1);
-
-        if (operation != "sqrt")
-        {
-            Console.WriteLine("Voer het tweede getal in:");
-            string input2 = Console.ReadLine();
-            if (string.IsNullOrWhiteSpace(input2))
+            //Checket inputvalidNumber1 voor geldigheid.
+            if (string.IsNullOrWhiteSpace(inputvalidNumber1))
             {
                 Console.WriteLine("Ongeldige invoer, geen getal.");
                 continue;
             }
-
-            for (int i = 0; i < input2.Length; i++)
+            for (int i = 0; i < inputvalidNumber1.Length; i++)
             {
-                if (input2[i] == '.')
+                if (inputvalidNumber1[i] == '.')
                 {
-                    if (hasDecimalPoint[1])
-                    {
-                        Console.WriteLine("Ongeldige invoer, geen getal.");
-                    }
-                    hasDecimalPoint[1] = true;
+                    hasDecimalPoint[0] = true;
                 }
-                else if (input2[i] == '-')
-                {
-                    if (i != 0)
-                    {
-                        Console.WriteLine("Ongeldige invoer, geen getal.");
-                    }
-                }
-                else if (!char.IsDigit(input2[i]))
+                if ((inputvalidNumber1[i] == '-' && i != 0) || !char.IsDigit(inputvalidNumber1[i]) || hasDecimalPoint[0])
                 {
                     Console.WriteLine("Ongeldige invoer, geen getal.");
                 }
             }
-        }
 
-        //kiest aan de hand van operatie
-        switch (operation)
-        {
-            case "+":
-                plus(number1, number2);
-                break;
-            case "-":
-                System.Console.WriteLine("-");
-                break;
-            case "*":
-                System.Console.WriteLine("*");
-                break;
-            case "/":
-                System.Console.WriteLine("/");
-                break;
-            case "%":
-                System.Console.WriteLine("%");
-                break;
-            case "18+":
-                System.Console.WriteLine("18+");
-                break;
-            case "sqrt":
-                System.Console.WriteLine("sqrt");
-                break;
-        }
+            validNumber1 = Convert.ToDouble(inputvalidNumber1);
 
-        if (operation == "-")
-        {
-            double temp = number1 - number2;
-            Console.WriteLine($"Resultaat: {number1} - {number2} = " + temp);
-        }
-        else if (operation == "*")
-        {
-            double temp = number1 * number2;
-            Console.WriteLine($"Resultaat: {number1} * {number2} = " + temp);
-        }
-        else if (operation == "/")
-        {
-            double temp = number1 / number2;
-            if (number2 != 0)
+            //checket inputvalidNumber2 voor geldigheid als de operatie niet sqrt is
+            if (operation != "sqrt")
             {
-                Console.WriteLine($"Resultaat: {number1} / {number2} = " + temp);
+                Console.WriteLine("Voer het tweede getal in:");
+                string inputvalidNumber2 = Console.ReadLine();
+                
+                if (string.IsNullOrWhiteSpace(inputvalidNumber2))
+                {
+                    Console.WriteLine("Ongeldige invoer, geen getal.");
+                    continue;
+                }
+                for (int i = 0; i < inputvalidNumber2.Length; i++)
+                {
+                    if (inputvalidNumber2[i] == '.')
+                    {
+                        hasDecimalPoint[0] = true;
+                    }
+                    if ((inputvalidNumber2[i] == '-' && i != 0) || !char.IsDigit(inputvalidNumber2[i]) || hasDecimalPoint[0])
+                    {
+                        Console.WriteLine("Ongeldige invoer, geen getal.");
+                    }
+                }
+                validNumber2 = Convert.ToDouble(inputvalidNumber2);
+            }
+
+            //kiest aan de hand van input operatie welke operatie het doet.
+            switch (operation)
+            {
+                case "+":
+                    plus(validNumber1, validNumber2);
+                    break;
+                case "-":
+                    min(validNumber1, validNumber2);
+                    break;
+                case "*":
+                    keer(validNumber1, validNumber2);
+                    break;
+                case "/":
+                    gedeeld(validNumber1, validNumber2);
+                    break;
+                case "%":
+                    modulo(validNumber1,validNumber2);
+                    break;
+                case "^":
+                    pow(validNumber1, validNumber2);
+                    break;
+                case "18+":
+                    ouderDan18(validNumber1, validNumber2);
+                    break;
+                case "sqrt":
+                    sqrt(validNumber1, validNumber2);
+                    break;
+                default:
+                    System.Console.WriteLine("Ongeldige operatie");
+                    break;
+            }
+            if (!vraagRetry()) break; // stopt calculator als antwoord niet "ja" is.
+        }
+        
+        // static functies
+        
+        static void plus(double validNumber1, double validNumber2)
+        {
+            double temp = validNumber1 + validNumber2;
+            Console.WriteLine($"Resultaat: {validNumber1} + {validNumber2} = " + temp);
+        }
+
+        static void min(double validNumber1, double validNumber2)
+        {
+            double temp = validNumber1 - validNumber2;
+            Console.WriteLine($"Resultaat: {validNumber1} - {validNumber2} = " + temp);
+        }
+
+        static void keer(double validNumber1, double validNumber2)
+        {
+            double temp = validNumber1 * validNumber2;
+            Console.WriteLine($"Resultaat: {validNumber1} * {validNumber2} = " + temp);
+        }
+
+        static void gedeeld(double validNumber1, double validNumber2)
+        {
+            if (validNumber2 != 0)
+            {
+                double temp = validNumber1 / validNumber2;
+                Console.WriteLine($"Resultaat: {validNumber1} / {validNumber2} = " + temp);
             }
             else
             {
                 Console.WriteLine("Delen door nul is niet toegestaan.");
             }
         }
-        else if (operation == "%")
+
+        static void modulo(double validNumber1, double validNumber2)
         {
-            double temp = number1 % number2;
-            Console.WriteLine($"Resultaat: {number1} % {number2} = " + temp);
+            Console.WriteLine($"Resultaat: {validNumber1} % {validNumber2} = " + (validNumber1 % validNumber2));
         }
-        else if (operation == "^")
+
+        static void pow(double validNumber1, double validNumber2)
         {
-            double temp = Math.Pow(number1, number2);
-            Console.WriteLine($"Resultaat: {number1} ^ {number2} = " + temp);
+            double temp = Math.Pow(validNumber1, validNumber2);
+            Console.WriteLine($"Resultaat: {validNumber1} ^ {validNumber2} = " + temp);
         }
-        else if (operation == "sqrt")
+
+        static void ouderDan18(double validNumber1, double validNumber2)
         {
-            double temp = Math.Sqrt(number1) + Math.Sqrt(number2);
-            Console.WriteLine($"Resultaat: √{number1} = " + temp);
-        }
-        else if (operation == "18+")
-        {
+            
             Console.WriteLine("Voer het jaartal in:");
             string year = Console.ReadLine();
+                
             if (string.IsNullOrWhiteSpace(year))
             {
                 Console.WriteLine("Ongeldige invoer, geen getal.");
-                continue;
+            }
+
+            for (int i = 0; i < year.Length && year.Length == 4; i++)
+            {
+                if (year[i] == '.' || year[i] == '-' || !char.IsDigit(year[i]))
+                {
+                    Console.WriteLine("Ongeldige invoer, geen jaartal.");
+                }
+            }
+
+            int inputDag = Convert.ToInt32(validNumber1);
+            int inputMaand = Convert.ToInt32(validNumber2);
+            int inputJaar = Convert.ToInt32(year);
+            
+            DateOnly currentDate = DateOnly.FromDateTime(DateTime.Now);
+            DateOnly inputDate = new DateOnly(inputJaar, inputMaand, inputDag);
+            
+            //doet -18 jaar op currentDate en vergelijkt dan of inputDate groter is dan CurrentDate
+            
+            if (currentDate.AddYears(-18) < inputDate)
+            {
+                Console.WriteLine($"Resultaat: jonger dan 18!");
             }
             else
             {
-                for (int i = 0; i < year.Length && year.Length == 4; i++)
-                {
-                    if (year[i] == '.')
-                    {
-                        Console.WriteLine("Ongeldige invoer, geen jaartal.");
-                        continue;
-                    }
-                    else if (year[i] == '-')
-                    {
-                        Console.WriteLine("Ongeldige invoer, geen jaartal.");
-                        continue;
-                    }
-                    else if (!char.IsDigit(year[i]))
-                    {
-                        Console.WriteLine("Ongeldige invoer, geen jaartal.");
-                    }
-                }
-
-                int temp1 = Convert.ToInt32(number1);
-                int temp2 = Convert.ToInt32(number2);
-                int temp3 = Convert.ToInt32(year);
-
-                DateOnly currentDate = DateOnly.FromDateTime(DateTime.Now);
-                if (currentDate.Year - temp3 <= 18)
-                {
-                    if (currentDate.Month <= temp2)
-                    {
-                        if (currentDate.Month == temp2)
-                        {
-                            if (currentDate.Day < temp1)
-                            {
-                                Console.WriteLine($"Resultaat: jonger dan 18!");
-                            }
-                            else
-                            {
-                                Console.WriteLine($"Resultaat: 18+");
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine($"Resultaat: jonger dan 18!");
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Resultaat: 18+");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"Resultaat: 18+");
-                }
+                Console.WriteLine($"Resultaat: 18+");
             }
+            
+            
         }
-        else
+
+        static void sqrt(double validNumber1, double validNumber2)
         {
-            Console.WriteLine("Ongeldige operatie.");
+            double temp = Math.Sqrt(validNumber1) + Math.Sqrt(validNumber2);
+            Console.WriteLine($"Resultaat: √{validNumber1} = " + temp);
         }
 
-        
-        Console.WriteLine("Wilt u doorgaan? (ja/nee)");
-        
-        //Stopt de loop als het antwoord niet ja is.
-        if (Console.ReadLine().ToLower() != "ja") break;
-        
-         
-    }
-
-    static void plus(double number1, double number2)
-    {
-        Console.WriteLine($"Resultaat: {number1} + {number2} = " + (number1+number2));
-    }
-    
-    static void keer(double number1, double number2)
-    {
-        Console.WriteLine($"Resultaat: {number1} * {number2} = " + (number1 * number2));
-    }
-
-    static void gedeeld(double number1, double number2)
-    {
-        if (number2 != 0)
+        static Boolean vraagRetry()
         {
-            Console.WriteLine($"Resultaat: {number1} / {number2} = " + number1/number2);
+            Console.WriteLine("Wilt u doorgaan? (ja/nee)");
+            //Returnt true als antwoord "ja" is op wilt u doorgaan.
+            return Console.ReadLine().ToLower() == "ja";
         }
-        else
-        {
-            Console.WriteLine("Delen door nul is niet toegestaan.");
-        }
-    }
 
-    static void modulo(double number1, double number2)
-    {
-        Console.WriteLine($"Resultaat: {number1} % {number2} = " + (number1%number2));
-    }
-    
-    static void ouderDan18(double number1, double number2)
-    {
-        Console.WriteLine($"Resultaat: {number1} % {number2} = " + (number1%number2));
-    }
-    
-    static void sqrt(double number1, double number2)
-    {
-        double temp = Math.Sqrt(number1) + Math.Sqrt(number2);
-        Console.WriteLine($"Resultaat: √{number1} = " + temp);
     }
 }
--- Learning/obj/Debug/net8.0/Learning.AssemblyInfo.cs
+++ Learning/obj/Debug/net8.0/Learning.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learning")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bb4943abf32b898b5fffa74e614bf18976eacb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9c03253385654cec18c094640eba7936b0eb505")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Learning")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
